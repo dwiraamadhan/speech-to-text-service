1615dc9963b16ac31579d6c52b6dfd10b5993eaa
Dwi Ramadhan <dwiramadhan50@gmail.com>
2024-03-22 23:06:42 +0700

    refactor : adding save audio to mongodb

--- functions/convert_audio_to_text.py
+++ functions/convert_audio_to_text.py
@@ -1,13 +1,20 @@
 import openai, os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
+from config.database import collection
 
 # Retrieve OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Convert audio to text
 def convert_audio_to_text(audio_file):
-    # try:
+        
+        #save audio file to mongodb
+        content = audio_file.read()
+        file_audio = {"audio_file": content}
+        collection.insert_one(file_audio)
+
+        # create object trascript
         transcript = openai.Client().audio.transcriptions.create(
             model="whisper-1",
             file=audio_file,
@@ -15,5 +22,3 @@ def convert_audio_to_text(audio_file):
         )
         print(transcript)
         return transcript
-    # except Exception as e:
-    #     return str(e)
