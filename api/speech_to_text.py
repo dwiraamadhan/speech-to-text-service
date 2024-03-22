90f86874c8f9194ec2d3110d741205562c14fa0c
Dwi Ramadhan <dwiramadhan50@gmail.com>
2024-03-22 23:11:28 +0700

    refactor: adding http exception

--- api/speech_to_text.py
+++ api/speech_to_text.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from dotenv import load_dotenv, find_dotenv
 from functions.convert_audio_to_text import convert_audio_to_text
 
@@ -8,11 +8,16 @@ load_dotenv(find_dotenv())
 # Create endpoint for speech-to-text
 @router.post("/speech-to-text")
 async def transcribe_audio(audio_file : UploadFile = File(...)):
-
-    with open(audio_file.filename, "wb") as buffer:
-        buffer.write(audio_file.file.read())
-    audio_input = open(audio_file.filename, "rb")
+    try:
+        # Open the audio file as a buffer
+        with open(audio_file.filename, "wb") as buffer:
+            buffer.write(audio_file.file.read())
+        audio_input = open(audio_file.filename, "rb")
     
-    message_decode = convert_audio_to_text(audio_input)
+        # Convert audio to text by calling convert_audio_to_text function
+        message_decode = convert_audio_to_text(audio_input)
 
-    return message_decode
+        return message_decode
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
