b04e1456e98cbd7e053277247fc5a8d8eebd1d85
Dwi Ramadhan <dwiramadhan50@gmail.com>
2024-03-22 23:08:27 +0700

    refactor: adding connection to mongodb

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
 from api.speech_to_text import router
+from pymongo.mongo_client import MongoClient
+
+# Create a new client and connect to the server
+uri = "mongodb://localhost:27017/"
+client = MongoClient(uri)
+
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command("ping")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 
 app = FastAPI()
 app.include_router(router)
